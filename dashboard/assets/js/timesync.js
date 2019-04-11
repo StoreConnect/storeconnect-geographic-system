916025113eb26bbcc2ac5f4b1867f4488f2962e8
Franck Truillot <franck.truillot@tevolys.com>
2019-04-11 12:15:04 +0200

    remove dashboard from repo

--- dashboard/assets/js/timesync.js
+++
@@ -1,7 +0,0 @@
-let spanTS = document.querySelector("#timesync");
-
-window.setInterval(function () {
-    fetch("http://apicapteur.westeurope.cloudapp.azure.com:3000/time_now").then(res => res.json()).then(function (response) {
-        spanTS.innerHTML = new moment(response.time_now).format("D/MM/YYYY HH:mm:s");
-    });
-}, 500);
